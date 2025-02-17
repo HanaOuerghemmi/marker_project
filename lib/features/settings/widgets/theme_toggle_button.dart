272541ba47ad23e9e3a9543813d67b5c220fdb5d
Hana Ouerghemmi <5004@holbertonstudents.com>
2025-02-17 16:01:27 +0100

    custom the theme button

--- lib/features/settings/widgets/theme_toggle_button.dart
+++ lib/features/settings/widgets/theme_toggle_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import '../../../core/core.dart';
 
 class ThemeToggleButton extends StatelessWidget {
@@ -9,14 +8,25 @@ class ThemeToggleButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeCubit = context.read<ThemeCubit>();
+    return BlocBuilder<ThemeCubit, ThemeMode>(
+      builder: (context, themeMode) {
+        // Define if the theme is dark mode or not.
+        bool isDarkMode = themeMode == ThemeMode.dark;
 
-    return ListTile(
-      title: Text(AppLocalizations.of(context)!.changeTheme),
-      trailing: IconButton(
-        icon: const Icon(Icons.brightness_6),
-        onPressed: () => themeCubit.toggleTheme(),
-      ),
+        return ListTile(
+          title: Text(AppLocalizations.of(context)!.changeTheme),
+          trailing: Switch(
+            value: isDarkMode,  // Set value based on theme mode
+            activeColor: Colors.amber,
+            inactiveThumbColor: Colors.grey,
+            inactiveTrackColor: Colors.grey.withOpacity(0.4),
+            activeTrackColor: Colors.amber.withOpacity(0.5),
+            onChanged: (bool newValue) {
+              context.read<ThemeCubit>().toggleTheme();
+            },
+          ),
+        );
+      },
     );
   }
 }
